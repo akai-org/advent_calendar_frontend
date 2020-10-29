aafbb4ee273535e4b7dc9ffbe61f2606ba16cc25
Damian Kalka <kalka762@gmail.com>
2020-10-29 13:10:19 +0100

    add page info in main container

--- src/components/AdventWindow/AdventWindow.js
+++ src/components/AdventWindow/AdventWindow.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { ReactComponent as Friends } from 'assets/svg/decorative_friends.svg';
-import { ReactComponent as SnowGames } from 'assets/svg/snow_games.svg';
 import { ReactComponent as Tree } from 'assets/svg/christmas_tree.svg';
 import { ReactComponent as Trees } from 'assets/svg/trees.svg';
+import { ReactComponent as AkaiLogo } from 'assets/svg/logo-color.svg';
+import { ReactComponent as QuestionMark } from 'assets/svg/question-mark.svg';
+import AdventInfoModal from './AdventInfoModal';
 
 const StyledAdventWindow = styled.div`
   position: absolute;
@@ -22,6 +23,8 @@ const StyledAdventWindow = styled.div`
     color: #bbb;
     font-size: 8rem;
     z-index: 4;
+    display: flex;
+    flex-direction: column;
 
     & > svg {
       position: absolute;
@@ -30,7 +33,7 @@ const StyledAdventWindow = styled.div`
       z-index: -1;
     }
 
-    & > svg:nth-child(1) {
+    & > .svg-1 {
       left: -15%;
       top: -30%;
       transform: rotate(170deg);
@@ -38,24 +41,59 @@ const StyledAdventWindow = styled.div`
       height: 400px;
     }
 
-    & > svg:nth-child(2) {
+    & > .svg-2 {
       right: -10%;
       bottom: -30%;
       width: 400px;
       height: 400px;
     }
   }
+
+  .info {
+    font-size: 1.5rem;
+    align-self: start;
+    margin-left: 12vw;
+    text-align: center;
+    vertical-align: middle;
+    display: flex;
+    align-items: center;
+  }
+
+  .info-icon {
+    height: 2rem;
+    width: 2rem;
+    margin-left: 5px;
+    fill: powderblue;
+    cursor: pointer;
+
+    & > svg {
+      height: 2rem;
+      width: 2rem;
+    }
+  }
 `;
 
 const AdventWindow = () => {
+  const [isModalVisible, setModalVisible] = useState(0);
+
   return (
-    <StyledAdventWindow>
-      <div>
-        Kalendarz Adwentowy
-        <Tree />
-        <Trees />
-      </div>
-    </StyledAdventWindow>
+    <>
+      <StyledAdventWindow>
+        <div>
+          Kalendarz Adwentowy
+          <span class='info'>
+            Created by
+            <a href='https://akai.org.pl' class='info-icon'>
+              <AkaiLogo />
+            </a>
+            <QuestionMark class='info-icon' onClick={() => setModalVisible(1)} />
+          </span>
+          <Tree class='svg-1' />
+          <Trees class='svg-2' />
+        </div>
+      </StyledAdventWindow>
+      <AdventInfoModal isModalVisible={isModalVisible} setModalVisible={setModalVisible} />
+    </>
   );
 };
 
